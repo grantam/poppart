70c5eb30ef3533fee9fc7d086d4592a89a64e697
Grant Arney Mitchell <MitchellG@ceu.edu>
2024-09-26 16:29:13 +0200

    more work

--- aggragte data cleaning.R
+++ aggragte data cleaning.R
@@ -18,8 +18,27 @@ data_start <- vdem %>%
   select(country_name, country_id, year, regturn = v2eltrnout, vapturn = v2elvaptrn, compvote = v2elcomvot, leg_ele = v2eltype_0, exe_ele = v2eltype_6, dist_mag = v2elloeldm)
 
 gov_party <- vparty %>%
-  select(country_id, country_text_id, year, anti_ele = v2paanteli, pro_ppl = v2papeople, mani = v2paopresp, v2xpa_popul, v2paseatshare) %>%
-  mutate(xi = 1-.25*(pro_ppl), yi = 1-.25*(anti_ele), zi = 1-.25*(mani), populism = 2/((1/xi) + (1/yi) + (1/zi)), repop = 2/((1/xi) + (1/yi)))
+  filter(v2pagovsup == 0) %>%
+  select(country_name, party_name = v2paenname, 
+         country_id, country_text_id, year, 
+         v2paid, anti_ele = v2paanteli, 
+         pro_ppl = v2papeople, mani = v2paopresp, 
+         v2xpa_popul, v2paseatshare)
 
 
-summary(gov_party)
+left_join()  
+  arrange(country_id, year) %>%
+  mutate(tenure_id = cumsum(v2paid != dplyr::lag(v2paid, default = first(v2paid))) + 1) %>%
+  group_by(tenure_id) %>%
+  summarise(country_id = first(country_id, na_rm = T),
+            name = first(party_name, na_rm = T),
+            e1 = first(year, na_rm = T),
+            e2 = last(year, na_rm = T),
+            fpop = first(v2xpa_popul, na_rm = T),
+            meanpop = mean(v2xpa_popul, na_rm = T),
+            fss = first(v2paseatshare, na_rm = T),
+            meanss = mean(v2paseatshare, na_rm = T),
+            term_num = n())
+
+left_join
+
