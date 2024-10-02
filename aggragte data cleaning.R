7b9c4185736e9858e877dffe3253af164190015e
Grant Arney Mitchell <MitchellG@ceu.edu>
2024-10-02 17:32:48 +0200

    stuff

--- aggragte data cleaning.R
+++ aggragte data cleaning.R
@@ -33,10 +33,11 @@ gov_party <- vparty %>%
          v2paid,
          v2xpa_popul, v2paseatshare)
 
-gov_data <- left_join(gov_party, data_start, by = c("year", "country_id")) %>%  
+gov_data_sep <- left_join(gov_party, data_start, by = c("year", "country_id")) %>%  
   arrange(country_id, year) %>%
   group_by(country_id) %>%
   mutate(tenure_id = cumsum(v2paid != dplyr::lag(v2paid, default = first(v2paid))) + 1) %>%
+  ungroup() %>%
   group_by(country_id, tenure_id) %>%
   mutate(pop0 = first(v2xpa_popul, na_rm = T),
          ss0 = first(v2paseatshare, na_rm = T),
@@ -48,11 +49,59 @@ gov_data <- left_join(gov_party, data_start, by = c("year", "country_id")) %>%
          pop_avg = mean(v2xpa_popul, na_rm = T))
   
 
-gov_data <- cbind(gov_data, 
-                  datawizard::demean(gov_data,
+gov_data_sep <- cbind(gov_data_sep, 
+                  datawizard::demean(gov_data_sep,
                                      select = c("v2x_polyarchy", "dist_mag", "term", "exe_ele", "leg_ele"), 
                                      group = "country_id")) %>%
   filter(v2x_polyarchy >= .42)
+
+
+data_start <- vdem %>%
+  select(country_name,
+         country_id, year,
+         regturn = v2eltrnout,
+         vapturn = v2elvaptrn,
+         compvote = v2elcomvot,
+         leg_ele = v2eltype_0,
+         exe_ele = v2eltype_6,
+         dist_mag = v2elloeldm,
+         v2x_polyarchy)
+
+
+
+gov_data_con <- left_join(gov_party, data_start, by = c("year", "country_id")) %>%  
+  arrange(country_id, year) %>%
+  group_by(country_id) %>%
+  mutate(tenure_id = cumsum(v2paid != dplyr::lag(v2paid, default = first(v2paid))) + 1,
+         lag_poly = dplyr::lag(v2x_polyarchy)) %>%
+  ungroup() %>%
+  group_by(tenure_id, country_id) %>%
+  summarise(pop0 = first(v2xpa_popul, na_rm = T),
+         ss0 = first(v2paseatshare, na_rm = T),
+         term = row_number(),
+         poly_first = first(v2x_polyarchy, na_rm = T),
+         mean_vap = mean(vapturn, na_rm = T),
+         comp_first = first(compvote, na_rm = T),
+         poly_last = last(lag_poly, na_rm = T))
+
+gov_data_con <- gov_data_con %>%
+  group_by(tenure_id, country_id) %>%
+  summarise(pop0 = first(pop0, na_rm = T),
+            ss0 = first(ss0, na_rm = T),
+            term = max(term),
+            poly_first = first(poly_first, na_rm = T),
+            mean_vap = mean(mean_vap, na_rm = T),
+            comp_first = first(comp_first, na_rm = T),
+            poly_last = last(poly_last, na_rm = T)) %>%
+  mutate(poly_chg = poly_first - poly_last)
+  
+
+
+gov_data_con <- cbind(gov_data_con, 
+                      datawizard::demean(gov_data_con,
+                                         select = c("poly_chg", "term"), 
+                                         group = "country_id")) %>%
+  filter(poly_first >= .42)
 
 data_start <- vdem %>%
   select(country_name,
@@ -95,7 +144,7 @@ m2 <- lmer(vapturn ~ system_pop_within + system_pop_between + exe_ele + leg_ele 
 summary(m2)
 
 
-stan_m1 <- stan_glmer(vapturn ~ pop0*v2x_polyarchy_within + v2x_polyarchy_between + exe_ele  + leg_ele + ss0 + term_within + term_between + as.factor(compvote) + (1 | country_id/tenure_id), data = gov_data, refresh = 0, cores = 8)
+stan_m1 <- stan_glmer(mean_vap ~ pop0 + poly_chg_within + poly_chg_between + term_within + term_between + as.factor(comp_first) + (1 | country_id), data = gov_data_con, refresh = 0, cores = 8)
 
 summary(stan_m1)
 
