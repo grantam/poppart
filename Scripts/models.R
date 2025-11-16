85210973d36db1a753346531d6ce780c4a503c5c
Grant Mitchell <141366794+grantam@users.noreply.github.com>
2025-11-15 23:58:16 -0800

    Update models.R
    splines

--- Scripts/models.R
+++ Scripts/models.R
@@ -17,6 +17,7 @@ library(stargazer)
 library(ordinal)
 library(data.table)
 library(corrplot)
+library(splines)
 
 #### Load data
 
@@ -332,7 +333,8 @@ m2a <- glmer(partyrep ~
              as.factor(lr_missing) +
              as.factor(gender) +
              as.factor(edu) +
-             (1 | country_id/year_fact),
+               time +
+             (1 + time | country_id/year_fact),
            data = cses_test, family = binomial(link = "logit"))
 
 summary(m2a)
@@ -360,10 +362,52 @@ m2b <- lmer(feel_max ~
                as.factor(lr_missing) +
                as.factor(gender) +
                as.factor(edu) +
-               (1 | country_id/year_fact),
+              time +
+              (1 + time | country_id) + (1 | country_id:year_fact),
              data = cses_test)
 
 summary(m2b)
+
+lme4::ranef(m2b)
+
+
+m2b_spline_rand <- lmer(
+  feel_max ~
+    lag_opp_pop_within_c +
+    lag_opp_pop_between_c +
+    lag_ipop_within_c +
+    lag_ipop_between_c +
+    v2elloeldm_c +
+    as.factor(v2elparlel) +
+    party_sys_age_c +
+    unemploy_t0_within_c +
+    unemploy_t0_between_c +
+    gini_within_c +
+    gini_between_c +
+    numofparties_within_c +
+    numofparties_between_c +
+    age_squ_c +
+    lr_within_c +
+    lr_between_c +
+    radical_within_c +
+    radical_between_c +
+    as.factor(lr_missing) +
+    as.factor(gender) +
+    as.factor(edu) +
+    spline1 + spline2 + spline3 +  # fixed effect for spline
+    (spline1 + spline2 + spline3 | country_id) +  # random slopes for country
+    (1 | country_id:year_fact),  # random intercept for country-election
+  data = cses_test
+)
+
+summary(m2b_spline_rand)
+
+mlist <- list(
+  "Time Model" = m2b,
+  "Spline Model" = m2b_spline_rand
+)
+
+modelsummary::modelsummary(mlist, output = "latex")
 
 ## Partisan Strength: Opposition only
 
